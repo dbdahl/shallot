4dbe774549ee9f0ce295668d612aad20f9f0d40c
David B. Dahl <dahl@stat.byu.edu>
2019-01-04 17:44:19 -0700

    Move code to the commonsMath library

--- R/shallot/R/onLoad.R
+++ R/shallot/R/onLoad.R
@@ -3,21 +3,12 @@
   s <- sdols:::s
   scalaJARs(pkgname,s)
   scalaLazy(function(s) s + '
-    import org.apache.commons.math3.random.{ RandomDataGenerator => RDG }
     import org.ddahl.shallot._
     import parameter._
     import parameter.decay._
     import parameter.partition._
     import distribution._
     import mcmc._
-
-    def rdg() = {
-      val ints = R.evalI1("as.integer(runif(2,-.Machine$integer.max,.Machine$integer.max))")
-      val seed = ((ints(0).asInstanceOf[Long]) << 32) | (ints(1) & 0xffffffffL)
-      val r = new RDG()
-      r.reSeed(seed)
-      r
-    }
 
     // This circumvents a bug in the class loader on some versions of Scala/JVM.
     scala.util.Try {
